e32765b975ec6b8e07d1f7134c6b971b1ddc0a1e
mdramirez071 <mdramirez071@gmail.com>
2019-06-11 16:12:49 -0700

    adds final versions of project files

---
+++ README.md
@@ -0,0 +1,11 @@
+This project is called "San Diego Top Spots" for module WEB102.
+
+The goal of this project was to accomplish the following:
+
+1.Create a table with three headings for San Diego's Top Spots including name, description, and location.
+2.By retrieving a JSON object, populate the table with all three attributes.
+3.Once the table is populated, assign a Google Maps link to the latitude and longitude coordinates provided in the JSON file.
+
+How to Demo this project:
+
+Clone the repository locally to your computer. Open up the project in Visual Studio Code. Open up Terminal, run the command "static ." //This will load the project into the web browser. Once the server is live, go to http://localhost:8080 in your browser. From there, all of the names, descriptions, and links will be displayed for each Top Spot in San Diego. To see where this top spot is located, click on the link provided to view that spot on Google Maps.
--- main.js
+++ main.js
@@ -1,4 +1,28 @@
-$(document).ready(function() {
+$(document).ready(function() { 
 // write your code here
 
+$.getJSON('data.json', function(data){
+   //console.log(data);
+   var item = [];
+   $.each(data,function(key,value){
+      //console.log(data);
+   item.push("<tr><td>" + value.name + '</td><td>' + value.description + "</td><td><a href='https://www.google.com/maps?q=" + value.location + "'>link</a></td></tr>" );
+   //console.log(item);
+   });
+   $('table').append(item);
 });
+ 
+});
+
+//All the lines below are used for TESTING PURPOSES ONLY: 
+
+ //The 3 lines below are supposed to insert the first name, description, and link into the first row within the table.
+   // $('#san-diego-top-spots tr > td:eq(0)').text(value.name).css("background-color", "red");
+   // $('#san-diego-top-spots tr > td:eq(1)').text(value.description).css("background-color", "green");
+   // $('#san-diego-top-spots tr > td:eq(2)').text(value.location).css("background-color", "grey");
+   // //this is printing out each "name" within the JSON object line by line (0 thru 29)   
+   // console.log(value.name); 
+   // //this is printing out each "description" within the JSON object line by line (0 thru 29)
+   // console.log(value.description);
+   // //this is printing out each "location" within the JSON object line by line (0 thru 29)
+   // console.log(value.location);
